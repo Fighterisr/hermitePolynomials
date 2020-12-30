eab36299ab96187fdceb7d8336b510774f8b1bc4
Omri Biton <fighterisr@gmail.com>
2020-12-30 02:54:04 +0200

    put the algorithm into a function

--- Main.py
+++ Main.py
@@ -1,21 +1,45 @@
-n=3
+# calculate and print a value using hermite polynomial using divided difference
+# the function takes a value to calculate, x array, fx array and fx derivative array
+def hermitePolynomial(value,x,fx,fxDerivative):
+    n = len(x)
+    z = [0 for i in range(2*n+1)]
+    q = [[0 for j in range(2*n)] for i in range(2*n)]
+
+    for i in range(n):
+        z[2*i] = z[2*i+1] = x[i]
+        q[2*i][0] = q[2*i+1][0] = fx[i]
+        q[2*i+1][1] = fxDerivative[i]
+
+        if i != 0:
+            q[2*i][1] = (q[2*i][0]-q[2*i-1][0])/(z[2*i]-z[2*i-1])
+
+    for i in range(2,2*n):
+        for j in range(2,i+1):
+            q[i][j] = (q[i][j-1]-q[i-1][j-1])/(z[i]-z[i-j])
+
+    # calculate the product for the hermite sum formula
+    def calcProduct(i, value, z):
+        product = 1
+        for j in range(i):
+            product = product * (value - z[j])
+        return product
+
+
+    # calculate the interpolation of value using hermite interpolation
+    def hermiteCalc(value, z, q, n):
+        sum = q[0][0]
+
+        for i in range(1, n):
+            sum = sum + (calcProduct(i, value, z) * q[i][i])
+
+        return sum
+
+    result = hermiteCalc(value,z,q,n)
+    print('The value of', value, 'is:', result)
+
+
 x = [1.3, 1.6, 1.9]
-y = [0.6200860, 0.4554022, 0.2818186]
-yDer = [-0.5220232, -0.5698959, -0.5811571]
-z = [0 for i in range(2*n+1)]
-q = [[0 for j in range(2*n)] for i in range(2*n)]
+fx = [0.6200860, 0.4554022, 0.2818186]
+fxDer = [-0.5220232, -0.5698959, -0.5811571]
 
-for i in range(n):
-    z[2*i] = z[2*i+1] = x[i]
-    q[2*i][0] = q[2*i+1][0] = y[i]
-    q[2*i+1][1] = yDer[i]
-
-    if i != 0:
-        q[2*i][1] = (q[2*i][0]-q[2*i-1][0])/(z[2*i]-z[2*i-1])
-
-for i in range(2,2*n):
-    for j in range(2,i+1):
-        q[i][j] = (q[i][j-1]-q[i-1][j-1])/(z[i]-z[i-j])
-
-
-
+hermitePolynomial(1.5,x,fx,fxDer)
